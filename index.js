fbc80ca9cfe1af9b9c949e887e848085a650a937
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-02-12 16:55:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,42 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = Math.floor(Math.random() * 100 + 1)
+ cart.push({[item]:price});
+ console.log (`${item} has been added to your cart.`)
+  return cart;
 }
 
 function viewCart() {
   // write your code here
+  const l = cart.length;
+
+  if (!l) {
+    return console.log("Your shopping cart is empty.");
+  }
+
+ let itemsAndPrices = [];
+
+  for (let i = 0; i < l; i++) {
+    let itemAndPrice = cart[i];
+    let item = Object.keys(itemAndPrice)[0];
+    let price = itemAndPrice[item];
+
+    itemsAndPrices.push(`${item} at \$${price}`);
+  }
+
+  switch(itemsAndPrices.length) {
+    case 1:
+      break;
+    case 2:
+      itemsAndPrices = itemsAndPrices.join(" and ");
+      break;
+    default:
+      itemsAndPrices[l-1] = "and ".concat(itemsAndPrices[l-1]);
+      itemsAndPrices = itemsAndPrices.join(", ");
+  }
+
+  console.log(`In your cart, you have ${itemsAndPrices}.`);
 }
 
 function total() {
